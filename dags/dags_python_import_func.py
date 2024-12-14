e42e759e0a7bbac76e52eccf12ef06bade0efc94
shj5978 <shj5978@gmail.com>
2024-12-14 18:08:22 +0900

    파이썬 import 들여쓰기 수정

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -6,13 +6,13 @@ import common.common_func import get_sftp
 
 
 with DAG(
-            dag_id="dags_python_import_func",
-                schedule="30 6 * * *",
-                    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-                        catchup=False
-                        ) as dag:
+    dag_id="dags_python_import_func",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
 
-        task_get_sftp = PythonOperator(
-                            task_id = 'task_get_sftp',
-                                        python_callable=get_sftp
-                                            )
+    task_get_sftp = PythonOperator(
+        task_id = 'task_get_sftp',
+        python_callable=get_sftp
+    )
