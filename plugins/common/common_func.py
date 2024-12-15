0dd4c9f2780ec3b3443d5167c2f526437e3be1d0
shj5978 <shj5978@gmail.com>
2024-12-15 14:30:26 +0900

    Args 변수 처리 에러 수정 2

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -2,6 +2,6 @@ def get_sftp():
     print('sftp 작업을 시작합니다.')
 
 def regist(name, sex, *args):
-    print('이름 : ' {name})
-    print('성별 : '{sex})
-    print('기타옵션들 : ' {args})
+    print('이름 : ' + {name})
+    print('성별 : ' + {sex})
+    print('기타옵션들 : ' + {args})
