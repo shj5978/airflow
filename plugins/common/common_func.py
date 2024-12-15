a300f244c64b68a202fb226d5ccef64f1d2f4d60
shj5978 <shj5978@gmail.com>
2024-12-15 14:56:28 +0900

    KwArgs 변수 처리

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -8,3 +8,15 @@ def regist(name, sex, *args):
     for arg in args:
         print(',' + arg)
     print(f'기타옵션들(2) : {args}')
+
+def regist2(name, sex, *args, **kwargs):
+    print(f'이름 : {name}')
+    print(f'성별 : {sex}')
+    print(f'기타옵션들 : {args}')
+    email = kwargs['email'] or None
+    phone = kwargs['phone'] or None
+    if email:
+        print(email)
+    if phone:
+        print(phone)
+    
