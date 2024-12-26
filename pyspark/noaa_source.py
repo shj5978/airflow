1868ac0b7fdadfb03a504a549d14400bbd1efdad
shj5978 <shj5978@gmail.com>
2024-12-26 16:13:01 +0900

    noaa 데이터 추가(13)

--- pyspark/noaa_source.py
+++ pyspark/noaa_source.py
@@ -18,7 +18,7 @@ print("Spark 세션 생성 완료")
     
 # S3 버킷에서 NOAA 데이터 경로
 print("S3 버킷에서 NOAA 데이터 경로 설정")
-s3_path = "s3a://noaa-ghcn-pds/csv.gz/"
+s3_path = "s3a://noaa-ghcn-pds/csv.gz/2024.csv.gz"
 
 # CSV 데이터 로드
 print("CSV 데이터 로드 시작")
