10417fbbe4c1a4303edc87d40ea891a83b8ad5ea
shj5978 <shj5978@gmail.com>
2024-12-26 14:29:03 +0900

    noaa 데이터 추가(3)

--- dags/dags_pyspark_noaa_test.py
+++ dags/dags_pyspark_noaa_test.py
@@ -19,7 +19,8 @@ with DAG(dag_id="dags_pyspark_noaa_test",
         application="/opt/airflow/pyspark/noaa_source.py",  # PySpark script 경로
         conn_id="spark_local",  # Spark 연결 ID
         name="make_source_task",
-        conf={"spark.master": "local[*]"}  # Spark master 설정
+        conf={"spark.master": "local[*]",
+              "spark.jars": "/opt/airflow/pyspark/jar/aws-java-sdk-1.11.1026.jar,/opt/airflow/pyspark/jar/hadoop-aws-3.3.1.jar"}  # Spark master 설정
     )
 
     make_source
