1b8f6cbd466b158a8fedf4f7abd8a12a6adc2269
shj5978 <shj5978@gmail.com>
2025-01-20 16:17:27 +0900

    minio 테스트(17)

--- dags/dags_pyspark_minio_test.py
+++ dags/dags_pyspark_minio_test.py
@@ -6,19 +6,20 @@ with DAG(dag_id="dags_pyspark_minio_test",
          tags=['spark'],
          start_date=datetime(2025, 1, 16),
          catchup=False,
-         schedule_interval=None) as dag :
+         schedule_interval=None) as dag:
 
-    # Preprocessing task
     make_source = SparkSubmitOperator(
         task_id="make_source_task",
-        application="/opt/airflow/pyspark/minio_source.py",  # PySpark script 경로
-        conn_id="spark_local",  # Spark 연결 ID
+        application="/opt/airflow/pyspark/minio_source.py",
+        conn_id="spark_local",
         name="make_source_task",
         conf={
-        "spark.master": "local[*]",
-        "spark.jars": "/opt/airflow/pyspark/jar/aws-java-sdk-bundle-1.12.497.jar,"
-                      "/opt/airflow/pyspark/jar/hadoop-aws-3.3.2.jar,"
-                      "/opt/airflow/pyspark/jar/hadoop-common-3.3.2.jar"
+            "spark.master": "local[*]",
+            "spark.jars": "/opt/airflow/pyspark/jar/aws-java-sdk-bundle-1.12.497.jar,"
+                          "/opt/airflow/pyspark/jar/hadoop-aws-3.3.2.jar,"
+                          "/opt/airflow/pyspark/jar/hadoop-common-3.3.2.jar",
+            "spark.hadoop.fs.s3a.path.style.access": "true",
+            "spark.hadoop.fs.s3a.connection.ssl.enabled": "false"
         }
     )
 
--- pyspark/minio_source.py
+++ pyspark/minio_source.py
@@ -1,8 +1,12 @@
 from pyspark.sql import SparkSession
+import logging
 
-# Spark 세션 생성
+# 로깅 설정
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("S3-to-MinIO")
+
 MAX_MEMORY = "16g"
-print("Spark 세션 생성 시작")
+logger.info("Spark 세션 생성 시작")
 spark = SparkSession.builder \
     .appName("S3-to-MinIO") \
     .config("spark.executor.memory", MAX_MEMORY) \
@@ -11,23 +15,21 @@ spark = SparkSession.builder \
     .config("spark.hadoop.fs.s3a.connection.maximum", "50") \
     .config("spark.hadoop.fs.s3a.access.key", "") \
     .config("spark.hadoop.fs.s3a.secret.key", "") \
-    .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
     .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider") \
+    .config("spark.hadoop.fs.s3a.endpoint", "s3.amazonaws.com") \
     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     .getOrCreate()
-print("Spark 세션 생성 완료")
+logger.info("Spark 세션 생성 완료")
 
-# 원격 S3 파일 경로
 s3_file_path = "s3a://noaa-ghcn-pds/csv.gz/by_station/ASN0000509*.csv.gz"
 
-# MinIO S3 설정
-minio_endpoint = "localhost:9000"  # MinIO 서버 주소
+minio_endpoint = "localhost:9000"
 minio_access_key = "NvqZkPJZsKTiPVFQczZo"
 minio_secret_key = "2N52qmlnEJ7zaj8pC8sGlhM1f2ZnKcfowlz1dvOZ"
-minio_bucket = "s3a://vm-workplace/uploaded_data/"  # MinIO 버킷 경로
+minio_bucket = "s3a://vm-workplace/uploaded_data"
 
-# MinIO 연결 설정 추가
+# MinIO 연결 설정
 spark.conf.set("spark.hadoop.fs.s3a.endpoint", minio_endpoint)
 spark.conf.set("spark.hadoop.fs.s3a.access.key", minio_access_key)
 spark.conf.set("spark.hadoop.fs.s3a.secret.key", minio_secret_key)
@@ -35,19 +37,16 @@ spark.conf.set("spark.hadoop.fs.s3a.path.style.access", "true")
 spark.conf.set("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")
 
 try:
-    # 원격 S3에서 데이터 읽기
-    print("S3에서 데이터 읽기 시작")
-    df = spark.read.csv(s3_file_path, header=False, inferSchema=True)
-    print("S3에서 데이터 읽기 완료")
+    logger.info("S3에서 데이터 읽기 시작")
+    df = spark.read.csv(s3_file_path, header=False, inferSchema=True, compression="gzip")
+    logger.info("S3에서 데이터 읽기 완료")
 
-    # MinIO에 데이터 저장
-    print("MinIO에 데이터 저장 시작")
+    logger.info("MinIO에 데이터 저장 시작")
     df.write.csv(minio_bucket, mode="overwrite", header=True)
-    print(f"데이터가 MinIO S3에 성공적으로 업로드되었습니다: {minio_bucket}")
+    logger.info(f"데이터가 MinIO에 성공적으로 업로드되었습니다: {minio_bucket}")
 
 except Exception as e:
-    print(f"Error while streaming data from S3 to MinIO: {e}")
+    logger.error(f"Error while streaming data from S3 to MinIO: {e}")
 
 finally:
-    # Spark 세션 종료
     spark.stop()
