d373c45378a342df3ff55987ee9cad484fae5e4c
shj5978 <shj5978@gmail.com>
2025-01-20 11:05:46 +0900

    minio 테스트(11)

--- pyspark/minio_source.py
+++ pyspark/minio_source.py
@@ -19,46 +19,39 @@ spark = SparkSession.builder \
     .getOrCreate()
 print("Spark 세션 생성 완료")
 
+# 원격 S3 파일 경로
 s3_file_path = "s3a://noaa-ghcn-pds/csv.gz/by_station/ASN0000509*.csv.gz"
-
-print("CSV 데이터 로드 시작")
-minio_df = spark.read.csv(
-    path=s3_file_path,
-    header=False,  # 헤더가 없는 파일임
-    inferSchema=True,  # 데이터 타입 자동 추론
-    sep=",",  # 쉼표로 데이터 구분
-)
-print("CSV 데이터 로드 완료")
 
 # MinIO 클라이언트 생성
 minio_client = Minio(
     "minio:9000",  # MinIO 서버 주소
-    "NvqZkPJZsKTiPVFQczZo",  # MinIO 접근 키
-    "2N52qmlnEJ7zaj8pC8sGlhM1f2ZnKcfowlz1dvOZ",  # MinIO 비밀 키
+    access_key="NvqZkPJZsKTiPVFQczZo",  # MinIO 접근 키
+    secret_key="2N52qmlnEJ7zaj8pC8sGlhM1f2ZnKcfowlz1dvOZ",  # MinIO 비밀 키
     secure=False  # HTTPS가 아닌 HTTP로 연결할 경우 False로 설정
 )
 print("MinIO 클라이언트 생성 완료")
 
 try:
-    # 컬럼 이름 추가
-    minio_df = minio_df.toDF("STATION", "DATE", "ELEMENT", "VALUE", "MFLAG", "QFLAG", "SFLAG", "ETC")
+    # S3에서 데이터 읽기
+    print("S3에서 데이터 읽기 시작")
+    csv_rdd = spark.sparkContext.textFile(s3_file_path)  # S3에서 데이터 읽기
+    csv_content = "\n".join(csv_rdd.collect())  # 데이터를 메모리에 수집
+    print("S3에서 데이터 읽기 완료")
 
-    # DataFrame을 메모리에 CSV로 저장
+    # 데이터 메모리에 로드
     csv_buffer = BytesIO()
-    pandas_df = minio_df.toPandas()  # Spark DataFrame을 Pandas DataFrame으로 변환
-    pandas_df.to_csv(csv_buffer, index=False)  # CSV로 변환하여 BytesIO에 저장
+    csv_buffer.write(csv_content.encode('utf-8'))  # 데이터를 바이너리로 변환
     csv_buffer.seek(0)  # 파일 포인터를 처음으로 이동
-    print("DataFrame 을 메모리에 csv 로 저장 완료")
 
     # MinIO에 파일 업로드
+    print("MinIO에 데이터 업로드 시작")
     minio_client.put_object(
         bucket_name="vm-workplace",  # MinIO 버킷 이름
         object_name="test_data/data.csv",  # MinIO에 저장될 경로 및 파일명
         data=csv_buffer,  # 바이너리 데이터
-        length=len(csv_buffer.getvalue()),  # 파일 크기
+        length=len(csv_content.encode('utf-8')),  # 데이터 크기
         content_type="application/csv"  # 파일 타입 지정
     )
-
     print("데이터가 MinIO에 성공적으로 업로드되었습니다.")
 
 except Exception as e:
