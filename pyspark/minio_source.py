dc99dc1eccac9c0ea67c8bf16fd10b0012424626
shj5978 <shj5978@gmail.com>
2025-01-20 09:55:34 +0900

    minio 테스트(6)

--- pyspark/minio_source.py
+++ pyspark/minio_source.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from pyspark.sql import SparkSession
 from minio import Minio
 
-MAX_MEMORY="16g"
+MAX_MEMORY = "16g"
 
 print("Spark 세션 생성 시작")
 spark = SparkSession.builder \
@@ -24,13 +24,13 @@ s3_file_path = "s3a://noaa-ghcn-pds/csv.gz/by_station/ASN0000509*.csv.gz"
 print("CSV 데이터 로드 시작")
 minio_df = spark.read.csv(
     path=s3_file_path,
-    header=False,           # 헤더가 없는 파일임
-    inferSchema=True,       # 데이터 타입 자동 추론
-    sep=","                # 쉼표로 데이터 구분
+    header=False,  # 헤더가 없는 파일임
+    inferSchema=True,  # 데이터 타입 자동 추론
+    sep=",",  # 쉼표로 데이터 구분
 )
 print("CSV 데이터 로드 완료")
 
-
+# MinIO 클라이언트 생성
 minio_client = Minio(
     "localhost:9001",  # MinIO 서버 주소
     "NvqZkPJZsKTiPVFQczZo",  # MinIO 접근 키
@@ -39,26 +39,25 @@ minio_client = Minio(
 )
 
 try:
+    # 컬럼 이름 추가
     minio_df = minio_df.toDF("STATION", "DATE", "ELEMENT", "VALUE", "MFLAG", "QFLAG", "SFLAG", "ETC")
 
-    # 출력: 데이터 프레임 스키마 확인
-    print("데이터 컬럼 정보:")
-    minio_df.printSchema()
-
-    # 데이터 예시 출력
-    print("데이터 예시:")
-    minio_df.show(10, truncate=False)
+    # DataFrame을 메모리에 CSV로 저장
+    csv_buffer = BytesIO()
+    pandas_df = minio_df.toPandas()  # Spark DataFrame을 Pandas DataFrame으로 변환
+    pandas_df.to_csv(csv_buffer, index=False)  # CSV로 변환하여 BytesIO에 저장
+    csv_buffer.seek(0)  # 파일 포인터를 처음으로 이동
 
     # MinIO에 파일 업로드
     minio_client.put_object(
-        'vm-workplace',  # MinIO 버킷 이름
-        'test_data/',  # MinIO에서의 저장 경로
-        BytesIO(minio_df),  # 파일 데이터
-        len(minio_df)  # 파일 크기
+        bucket_name="vm-workplace",  # MinIO 버킷 이름
+        object_name="test_data/data.csv",  # MinIO에 저장될 경로 및 파일명
+        data=csv_buffer,  # 바이너리 데이터
+        length=len(csv_buffer.getvalue()),  # 파일 크기
+        content_type="application/csv"  # 파일 타입 지정
     )
 
-        # 저장 확인
-    print(f"데이터가 {output_path} 경로에 Parquet 형식으로 저장되었습니다.")
+    print("데이터가 MinIO에 성공적으로 업로드되었습니다.")
 
 except Exception as e:
     print(f"Error while streaming data from S3 to MinIO: {e}")
