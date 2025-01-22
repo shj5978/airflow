f7e24458f3cb63725d45dc1119a83536edde97a8
shj5978 <shj5978@gmail.com>
2025-01-22 10:05:33 +0900

    49

--- pyspark/minio_source.py
+++ pyspark/minio_source.py
@@ -41,16 +41,16 @@ print("MinIO 클라이언트 설정 완료.")
 
 try:
     # 페이지네이션 설정: 다음 페이지가 있는지 확인하기 위해 계속 요청
-    continuation_token = None
-    while True:
+    #continuation_token = None
+    #while True:
         # S3에서 파일 목록 가져오기
         list_params = {
             "Bucket": aws_s3_bucket_name,
             "Prefix": aws_s3_folder,
         }
         
-        if continuation_token:
-            list_params["ContinuationToken"] = continuation_token
+        #if continuation_token:
+        #    list_params["ContinuationToken"] = continuation_token
         
         s3_objects = s3_client.list_objects_v2(**list_params)
 
@@ -80,9 +80,9 @@ try:
                 print(f"Uploaded {file_key} to MinIO at {minio_target_path}")
 
         # 페이지네이션: 다음 페이지가 있으면 토큰 갱신
-        continuation_token = s3_objects.get("NextContinuationToken")
-        if not continuation_token:
-            break  # 더 이상 페이지가 없으면 종료
+        #continuation_token = s3_objects.get("NextContinuationToken")
+        #if not continuation_token:
+        #    break  # 더 이상 페이지가 없으면 종료
 
 except Exception as e:
     print(f"오류 발생: {e}")
