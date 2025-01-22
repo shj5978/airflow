3b10cffc3a28e2937cd0898ed60b9654bd838101
shj5978 <shj5978@gmail.com>
2025-01-22 13:45:32 +0900

    57

--- dags/dags_boto3_minio_test.py
+++ dags/dags_boto3_minio_test.py
@@ -8,10 +8,10 @@ with DAG(dag_id="dags_boto3_minio_test",
          catchup=False,
          schedule_interval=None) as dag:
 
-    make_source = BashOperator(
-        task_id="make_source_task",
-        bash_command="python3 /opt/airflow/pyspark/boto3_source.py",
-    )
+    # make_source = BashOperator(
+    #     task_id="make_source_task",
+    #     bash_command="python3 /opt/airflow/pyspark/boto3_source.py",
+    # )
 
     upload_source = BashOperator(
         task_id="upload_source_task",
