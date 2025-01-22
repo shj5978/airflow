4cac06bbdde6c925c8fc1e3953a22f9b14d99a35
shj5978 <shj5978@gmail.com>
2025-01-22 16:03:36 +0900

    68

--- pyspark/minio_upload.py
+++ pyspark/minio_upload.py
@@ -20,38 +20,38 @@ minio_client = Minio(
 print("MinIO 클라이언트 설정 완료.")
 ############################# MIN IO 설정 ################################################
 
-try:
-
-    # Bucket 존재 여부 확인
-    if minio_client.bucket_exists(minio_bucket_name):
-        print(f"Bucket '{minio_bucket_name}' exists.")
-    else:
-        print(f"Bucket '{minio_bucket_name}' does not exist.")
-
-except Exception as e:
-    print(f"오류 발생: {e}")
-
-
 # try:
-#     ############################# MIN IO 에 업로드 ################################################
-#     for root, _, files in os.walk(source_folder):
-#         for file_name in files:
-#             local_file_path = os.path.join(root, file_name)
-#             minio_target_path = os.path.relpath(local_file_path, source_folder).replace("\\", "/")
 
-#             try:
-#                 # fput_object로 파일 업로드
-#                 minio_client.fput_object(
-#                     bucket_name=minio_bucket_name,
-#                     object_name=minio_target_path,
-#                     file_path=local_file_path
-#                 )
-#                 print(f"Uploaded {local_file_path} to MinIO at {minio_target_path}")
-#             except Exception as e:
-#                 print(f"Failed to upload {local_file_path}: {e}")
-#     ############################# MIN IO 에 업로드 ################################################
-
-#     print("처리 완료!!!!!!")
+#     # Bucket 존재 여부 확인
+#     if minio_client.bucket_exists(minio_bucket_name):
+#         print(f"Bucket '{minio_bucket_name}' exists.")
+#     else:
+#         print(f"Bucket '{minio_bucket_name}' does not exist.")
 
 # except Exception as e:
 #     print(f"오류 발생: {e}")
+
+
+try:
+    ############################# MIN IO 에 업로드 ################################################
+    for root, _, files in os.walk(source_folder):
+        for file_name in files:
+            local_file_path = os.path.join(root, file_name)
+            minio_target_path = os.path.relpath(local_file_path, source_folder).replace("\\", "/")
+
+            try:
+                # fput_object로 파일 업로드
+                minio_client.fput_object(
+                    bucket_name=minio_bucket_name,
+                    object_name=minio_target_path,
+                    file_path=local_file_path
+                )
+                print(f"Uploaded {local_file_path} to MinIO at {minio_target_path}")
+            except Exception as e:
+                print(f"Failed to upload {local_file_path}: {e}")
+    ############################# MIN IO 에 업로드 ################################################
+
+    print("처리 완료!!!!!!")
+
+except Exception as e:
+    print(f"오류 발생: {e}")
